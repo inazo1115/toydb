1d2f3fe24f8fd59f93ee8c50308a45099675ba3b
inazo1115 <inaba1115@gmail.com>
2017-01-03 23:12:35 +0900

    Add frame tests

--- lib/storage/frame.go
+++ lib/storage/frame.go
@@ -4,48 +4,77 @@ import (
 	"github.com/inazo1115/toydb/lib/page"
 )
 
+// Frame is the model of buffer which is on the memory. Frame stores a page and
+// some information of the cache control.
 type Frame struct {
 	page     *page.DataPage
 	pinCount int32
 	dirty    bool
+	hitCount int32 // hitCount is used by cache control strategy.
 }
 
+// NewFrame creates a new frame and returns it's pointer.
 func NewFrame(page *page.DataPage) *Frame {
-	return &Frame{page, 0, false}
+	return &Frame{page, 0, false, 0}
 }
 
+// Page is the getter of the page.
 func (f *Frame) Page() *page.DataPage {
 	return f.page
 }
 
+// SetPage is the setter of the page.
 func (f *Frame) SetPage(page *page.DataPage) {
 	f.page = page
 }
 
+// DeletePage deletes the reference of the page which had been stored.
 func (f *Frame) DeletePage() {
 	f.page = nil
 }
 
+// PinCount is the setter of the pinCount.
 func (f *Frame) PinCount() int32 {
 	return f.pinCount
 }
 
-func (f *Frame) IncPinCount() {
+// Pin increments pin counts.
+func (f *Frame) Pin() {
 	f.pinCount++
 }
 
-func (f *Frame) DecPinCount() {
+// Pin decrements pin counts.
+func (f *Frame) UnPin() {
 	f.pinCount--
 }
 
+// Dirty is the getter of the dirty. If this parameter is true, the page has
+// been modified since load from disk.
 func (f *Frame) Dirty() bool {
 	return f.dirty
 }
 
+// TurnOnDirty sets the dirty true.
 func (f *Frame) TurnOnDirty() {
 	f.dirty = true
 }
 
+// TurnOnDirty sets the dirty false.
 func (f *Frame) TurnOffDirty() {
 	f.dirty = false
+}
+
+// HitCount is the getter of the hitCount.
+func (f *Frame) HitCount() int32 {
+	return f.hitCount
+}
+
+// IncHitCount increments the hit counts. This represents the cache hit.
+func (f *Frame) IncHitCount() {
+	f.hitCount++
+}
+
+// SetHitCount is the setter of the hitCount.
+func (f *Frame) SetHitCount(hitCount int32) {
+	f.hitCount = hitCount
 }
