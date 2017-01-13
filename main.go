511a960eceeffebd3b31422c496a74f9fee55ff2
inazo1115 <inaba1115@gmail.com>
2017-01-14 01:38:58 +0900

    refactor

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 	query2 := "select * from table_name"
 
 	c := client.NewClient()
-	c.Query(query0)
 	fmt.Println(query0)
+	c.Query(query0)
 	for i := 0; i < 5000; i++ {
 		q := fmt.Sprintf(query1, "\"foo\"", i, 200)
 		fmt.Println(q)
 		c.Query(q)
 	}
-	c.Query(query2)
 	fmt.Println(query2)
+	c.Query(query2)
 }
